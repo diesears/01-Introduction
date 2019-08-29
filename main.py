3eae635561cbe93dfdfa60e1488e9e258f0a3d22
Dietrich <diesears@iu.edu>
2019-08-29 14:21:45 -0400

    Completes the assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, My name is Dietrich Sears')
+print('My favorite game is the original Star Wars Battlefront')
+print('My concerns with this class are keeping up to pace with the new material and feeling comfotable with new skills')
+print('Im excited about The Rise of Skywalker in December, also fighting climate change')
+print('My stackoverflow.com user number is 11991280')
+print('The URL to my github.com profile is https://github.com/diesears')
